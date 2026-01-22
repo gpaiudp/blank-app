063b6a8e728b7bdc6978a1b23ddbf41cbfd0b8aa
GIRIDHAR PAI <77011546+gpaiudp@users.noreply.github.com>
2026-01-22 14:26:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,3 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import numpy as np
-
-# Create a figure and axis
-fig, ax = plt.subplots()
-ax.set_xlim((0, 2))
-ax.set_ylim((-2, 2))
-line, = ax.plot([], [], lw=2)
-
-# Define the animation function
-def animate(i):
-    # Compute the x and y coordinates for the i-th frame
-    x = np.linspace(0, 2, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    # Set the data for the line object
-    line.set_data(x, y)
-    return line,
-
-# Create the animation object
-# frames=100 specifies the number of frames
-# interval=20 is the delay in milliseconds between frames
-anim = animation.FuncAnimation(fig, animate, frames=100, interval=20, blit=True)
-
-# Show the plot
-plt.show()
 
 
 
